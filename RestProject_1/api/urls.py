a80a50ad7a66b7f062012a88a4bf698fff1c86a6
rahman <rahman821.ar@gmail.com>
2025-01-14 10:31:43 +0530

    added router and viewSet

--- RestProject_1/api/urls.py
+++ RestProject_1/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import get_data,addItem
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet) 
 
 urlpatterns = [
     path('', get_data),
     path('add/',addItem),
+    path('router/',include(router.urls)),
 ]
--- RestProject_1/api/views.py
+++ RestProject_1/api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ItemSerializer,Item
+from rest_framework import viewsets
 
 
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
 
 @api_view(['GET'])
